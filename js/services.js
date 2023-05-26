3eddcc6e481faca29acb7948ff3ecf447cffb640
Chumak Ivan <66885660+BAHOking@users.noreply.github.com>
2023-05-26 13:34:57 +0300

    1
    1

--- js/services.js
+++
@@ -1,7 +0,0 @@
-let servicesMoreBtn = document.querySelector(".services__more-btn");
-let servicesItemsBox = document.querySelector(".services__items-box");
-
-servicesMoreBtn.addEventListener("click", function showHideServices() {
-  servicesItemsBox.classList.toggle("show-hide");
-  servicesMoreBtn.classList.toggle("services__more-btn--rotate");
-});
